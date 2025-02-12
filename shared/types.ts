da28fd01c1418157b3c6050f190686c4409db8b8
Neacsu Flavius Catalin <kete102@gmail.com>
2025-02-12 23:16:44 +0100

    fix ErroResponse type

--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod"
+
 //Default success response with generic optinal data
 export type SuccessResponse<T = void> = {
 	success: true
@@ -7,4 +9,14 @@ export type SuccessResponse<T = void> = {
 export type ErrorResponse = {
 	success: false
 	error: string
+	isFormError?: boolean
 }
+
+export const loginSchema = z.object({
+	username: z
+		.string()
+		.min(3)
+		.max(31)
+		.regex(/^[a-zA-Z0-9_]+$/),
+	password: z.string().min(3).max(255),
+})
